ada76fa2d9d03088002dc89b43b0e7913fd978f5
mohammadtahsin <mohammadtahsin@carfax.com>
2023-07-30 14:33:07 -0400

    add basic routes

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-import { Router } from "@kapsonfire/bun-bakery";
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+import { logger } from "hono/logger";
 
-new Router({
-  port: 3000,
-  assetsPath: import.meta.dir + "/assets/",
-  routesPath: import.meta.dir + "/routes/",
+let clipboard = null;
+
+const app = new Hono();
+app.use("*", logger());
+app.use("*", serveStatic({ root: "/assets" }));
+
+app.post("/api/clipboard", async (c) => {
+  const body = await c.req.json<{
+    data: string;
+  }>();
+  console.log(body);
+
+  return c.json({ success: true });
 });
+
+app.get("/api/clipboard", (c) => {
+  return c.json({ data: clipboard });
+});
+
+export default app;
